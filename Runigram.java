c1a3d54d353407fb64bdf535bcf3331f4f813530
Noa Sufrin <noa.sufrin@post.runi.ac.il>
2024-12-20 10:21:52 +0200

    without pause

--- Runigram.java
+++ Runigram.java
@@ -242,7 +242,6 @@ public class Runigram {
 		for (int i = 0; i < n; i++) {
 			double alpha = (double) (n - i) / (double) n;
 			display(blend(source, newImage, alpha));
-			StdDraw.pause(500);
 		}
 	}
 	
